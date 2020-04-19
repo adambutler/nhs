89a86067c0eefd83d615000c4dd965aa6c66a618
Adam Butler <adam@lab.io>
2020-04-19 11:24:48 +0100

    patch

--- app/controllers/frames_controller.rb
+++ app/controllers/frames_controller.rb
@@ -1,4 +1,6 @@
 class FramesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     @frames = Frame.where(approved: !(params[:review].present?))
   end
